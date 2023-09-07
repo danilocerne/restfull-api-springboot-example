99d971766503f71d06ebaf298446f9925f197aba
Danilo Cerne <prog.danilocerne@gmail.com>
2023-09-06 22:12:15 -0300

    Add http verbs to CRUD of Person.

--- restfull-api-springboot-example/src/main/java/br/com/danilocerne/springboot/example/services/PersonServices.java
+++ restfull-api-springboot-example/src/main/java/br/com/danilocerne/springboot/example/services/PersonServices.java
@@ -1,5 +1,8 @@
 package br.com.danilocerne.springboot.example.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Service;
@@ -8,8 +11,21 @@ import br.com.danilocerne.springboot.example.models.Person;
 
 @Service
 public class PersonServices {
+	
+	private final AtomicLong counter = new AtomicLong();
 
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	public List<Person> findAll() {
+		logger.info("Finding all people!");
+		
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
 	
 	public Person findById(String id) {
 		
@@ -23,5 +39,36 @@ public class PersonServices {
 		person.setAddress("Ribeirão Preto - São Paulo - Brasil");
 		person.setGender("Masculino");
 		return person;
+	}
+	
+	private Person mockPerson(int i) {
+		
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLastName("Last name " + i);
+		person.setAddress("Some address in Brazil " + i);
+		person.setGender("Masculino");
+		return person;
+	}
+	
+	public Person create(Person person) {
+		
+		logger.info("Creating one person!");
+
+		return person;
+	}
+	
+	public Person update(Person person) {
+		
+		logger.info("Updating one person!");
+
+		return person;
+	}
+	
+	public void delete(String id) {
+		
+		logger.info("Deleting one person!");
+
 	}
 }
